04a2980a4e52ca79ad518eec69aca7964737b05c
Ewa Szymik <ewaszymik25@gmail.com>
2024-11-29 09:47:59 +0100

    unused directive removed

--- app/src/main/java/com/example/minutnik/FragmentTimer.kt
+++ app/src/main/java/com/example/minutnik/FragmentTimer.kt
@@ -1,7 +1,6 @@
 package com.example.minutnik
 
 import android.os.Bundle
-import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
